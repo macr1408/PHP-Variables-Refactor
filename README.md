c631667a765be7c60e89ad94c8d18d28859d0db9
Marcos Carpio <macr1408@hotmail.com>
2024-09-26 01:50:28 -0300

    1.1.0 Release

--- README.md
+++ README.md
@@ -13,14 +13,21 @@ A simple extension for refactoring PHP function calls into variables right from 
 This extension contributes the following settings:
 
 - `phpVariablesRefactor.naming.type`: Choose your preferred naming type. Available options are `lowerCamelCase`, `CamelCase`, `snake_case` and `kebab-case`. Default is `lowerCamelCase`
+- `phpVariablesRefactor.naming.variablesQuantity`: Choose your preferred amount of variables to use as possible variable name.
 
 ## Contributing
 
 I developed this extension with the intention of it being freely available, open source, and supported collectively. Therefore, please don't anticipate significant updates from me beyond critical bug fixes.
 
-If you have a suggestion for a new feature, I'm open to considering it. However, it's worth noting that I'm not particularly proficient in TypeScript (as you may have noticed if you've looked at the source code). That said, I encourage anyone with the expertise to create a pull request, and if it aligns with the project's goals, I'll gladly support merging it.
+If you have a suggestion for a new feature, I'm open to considering it. I encourage anyone with the expertise to create a pull request, and if it aligns with the project's goals, I'll gladly support merging it.
 
 ## Release Notes
+
+### 1.1.0
+
+- Added support for multiple variables name hint
+- Fix identation for some cases
+- Improved codebase
 
 ### 1.0.0
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { workspace } from 'vscode';
 const COMMAND = 'php-variables-refactor.refactorVariable';
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.languages.registerCodeActionsProvider(
-      'php',
-      new VariableRefactor(),
-      {
-        providedCodeActionKinds: VariableRefactor.providedCodeActionKinds,
-      }
-    )
+    vscode.languages.registerCodeActionsProvider('php', new VariableRefactor(), {
+      providedCodeActionKinds: VariableRefactor.providedCodeActionKinds,
+    })
   );
 
   context.subscriptions.push(
@@ -21,34 +18,36 @@ export function activate(context: vscode.ExtensionContext) {
 
       if (editor) {
         const document = editor.document;
-        let selection = editor.selection;
+        const selection = editor.selection;
+        const workspaceConfiguration = workspace.getConfiguration('editor');
 
         const variableValues = await getVariableValues(document, selection);
         if (!variableValues.name) {
           return;
         }
 
-        const { currentFirstPosition, aboveLastPosition, currentLine } =
-          await getLinesInformation(document, selection);
+        let currentLine = document.lineAt(selection.anchor.line);
+        const lineEnding: string = workspaceConfiguration.get<string>('lineEnding') ?? '\n';
+
+        let insertPosition = new vscode.Position(currentLine.lineNumber, 0);
+        let identation = currentLine.text.substring(0, currentLine.firstNonWhitespaceCharacterIndex);
+
+        while (lineEndsWithComma(currentLine.text)) {
+          currentLine = document.lineAt(currentLine.lineNumber - 1);
+
+          insertPosition = new vscode.Position(currentLine.lineNumber, 0);
+          identation = currentLine.text.substring(0, currentLine.firstNonWhitespaceCharacterIndex);
+        }
 
         await editor.edit((editBuilder) => {
-          editBuilder.insert(aboveLastPosition, '\n');
-        });
-
-        // refreshing selection is needed to prevent bugs
-        selection = editor.selection;
-        await editor.edit((editBuilder) => {
           editBuilder.insert(
-            currentFirstPosition,
-            ' '.repeat(currentLine.firstNonWhitespaceCharacterIndex)
+            insertPosition,
+            identation + `\$${variableValues.name} = ${variableValues.value};` + lineEnding
           );
 
-          editBuilder.insert(
-            currentFirstPosition,
-            `\$${variableValues.name} = ${variableValues.value};`
-          );
-
-          editBuilder.replace(selection, `\$${variableValues.name}`);
+          for (const selection of editor.selections) {
+            editBuilder.replace(selection, `\$${variableValues.name}`);
+          }
         });
       }
     })
@@ -76,44 +75,23 @@ async function getVariableValues(
   };
 }
 
-async function getLinesInformation(
-  document: vscode.TextDocument,
-  selection: vscode.Selection
-): Promise<{
-  currentFirstPosition: vscode.Position;
-  aboveLastPosition: vscode.Position;
-  currentLine: vscode.TextLine;
-}> {
-  const currentLine = document.lineAt(selection.anchor.line);
-  const currentFirstPosition = new vscode.Position(
-    currentLine.lineNumber,
-    currentLine.firstNonWhitespaceCharacterIndex
-  );
-  const aboveLastPosition = new vscode.Position(
-    currentLine.lineNumber - 1,
-    document.lineAt(currentLine.lineNumber - 1).range.end.character
-  );
-
-  return {
-    currentFirstPosition,
-    aboveLastPosition,
-    currentLine,
-  };
+function lineEndsWithComma(line: string): boolean {
+  return line.replace(/[\s\t\n]+$/, '').endsWith(',');
 }
 
-function inferVariableName(value: string) {
+function inferVariableName(value: string): string {
   const defaultValue = 'variable';
 
   // $var->getProperty() = ['var', 'Property'];
   let matches = value.match(/\$(\w*\d*)->get_*(\w*\d*)\([^)]*\);*$/i);
   if (matches) {
-    return transformVariableIntoConfigCase(matches);
+    return transformVariableIntoConfigCase(matches.slice(1));
   }
 
   // $var->property = ['var', 'Property'];
   matches = value.match(/\$(\w*\d*)->_*(\w*\d*)\;*$/i);
   if (matches) {
-    return transformVariableIntoConfigCase(matches);
+    return transformVariableIntoConfigCase(matches.slice(1));
   }
 
   // $var->getProperty1()->getProperty2() = ['Property1', 'Property2'];
@@ -132,57 +110,41 @@ function inferVariableName(value: string) {
 }
 
 function transformVariableIntoConfigCase(variablePieces: string[]): string {
-  while (variablePieces.length > 2) {
-    variablePieces.shift();
-  }
-
   if (variablePieces.length === 1) {
     return variablePieces[0];
   }
 
-  const config: { type: string } | undefined = vscode.workspace
-    .getConfiguration('phpVariablesRefactor')
-    .get('naming');
+  const config = vscode.workspace.getConfiguration('phpVariablesRefactor');
 
-  switch (config?.type) {
+  const variablesQuantity = config.get('naming.variablesQuantity') ?? 2;
+  variablePieces = variablePieces.slice((variablesQuantity as number) * -1);
+
+  const namingType = config.get('naming.type');
+  switch (namingType) {
     default:
     case undefined:
     case 'lowerCamelCase':
-      return (
-        variablePieces[0].toLowerCase() +
-        variablePieces[1].charAt(0).toUpperCase() +
-        variablePieces[1].slice(1)
-      );
+      return variablePieces
+        .map((piece, index) => {
+          return index === 0 ? piece.toLowerCase() : piece.charAt(0).toUpperCase() + piece.slice(1);
+        })
+        .join('');
 
     case 'CamelCase':
-      return (
-        variablePieces[0].charAt(0).toUpperCase() +
-        variablePieces[0].slice(1) +
-        variablePieces[1].charAt(0).toUpperCase() +
-        variablePieces[1].slice(1)
-      );
+      return variablePieces.map((piece) => piece.charAt(0).toUpperCase() + piece.slice(1)).join('');
 
     case 'snake_case':
-      return (
-        variablePieces[0].toLowerCase() + '_' + variablePieces[1].toLowerCase()
-      );
+      return variablePieces.map((piece) => piece.toLowerCase()).join('_');
 
     case 'kebab-case':
-      return (
-        variablePieces[0].toLowerCase() + '-' + variablePieces[1].toLowerCase()
-      );
+      return variablePieces.map((piece) => piece.toLowerCase()).join('-');
   }
 }
 
 export class VariableRefactor implements vscode.CodeActionProvider {
-  public static readonly providedCodeActionKinds = [
-    vscode.CodeActionKind.QuickFix,
-  ];
+  public static readonly providedCodeActionKinds = [vscode.CodeActionKind.QuickFix];
 
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range
-  ): vscode.CodeAction[] {
+  public provideCodeActions(document: vscode.TextDocument, range: vscode.Range): vscode.CodeAction[] {
     if (!this.isValidVariableValue(document, range)) {
       return [];
     }
@@ -190,13 +152,8 @@ export class VariableRefactor implements vscode.CodeActionProvider {
     return [this.createFix('Refactor as variable')];
   }
 
-  private isValidVariableValue(
-    document: vscode.TextDocument,
-    range: vscode.Range
-  ): boolean {
-    const selection = document.getText(
-      vscode.window.activeTextEditor?.selection
-    );
+  private isValidVariableValue(document: vscode.TextDocument, range: vscode.Range): boolean {
+    const selection = document.getText(vscode.window.activeTextEditor?.selection);
 
     if (!selection) {
       return false;
